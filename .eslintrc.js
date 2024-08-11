b554a12eba8dcd056c58cf856a77281cc884fa61
Jeral Pineda <jeral.cris@gmail.com>
2024-08-11 15:26:54 -0600

    fix: disabled eslint errors for inline style
    disabled error eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,15 @@ module.exports = {
   root: true,
   extends: "@react-native",
   rules: {
-    "prettier/prettier": 0,
     quotes: [2, "double"],
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
+    "react-native/no-inline-styles": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        "no-inline-styles": false,
+      },
+    ],
   },
 };
