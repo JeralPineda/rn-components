280747d9d6a0bc81b5b929ee21715f206bdcb302
Jeral Pineda <jeral.cris@gmail.com>
2024-08-11 13:42:56 -0600

    fix: add icons for app
    add icons

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -2,7 +2,7 @@ import "react-native-gesture-handler";
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Navigator } from "./presentation/navigator/Navigation";
+import { Navigator } from "./presentation/navigator/Navigator";
 
 function ComponentsApp(): React.JSX.Element {
   return (
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { MyIcon } from "../../icons/Icons";
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <MyIcon />
     </View>
   );
 };
