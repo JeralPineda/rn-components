c9f32c9aca57ac41150271aaf7c26cb7608a84a4
Jeral Pineda <jeral.cris@gmail.com>
2024-08-14 23:26:40 -0600

    feat: add custom components
    Add components

---
+++ src/presentation/components/ui/Button.tsx
@@ -0,0 +1,34 @@
+import { Pressable, StyleProp, Text, ViewStyle } from "react-native";
+import { colors, globalStyles } from "../../../config/theme/theme";
+
+interface ButtonProps {
+  text: string;
+  styles?: StyleProp<ViewStyle>;
+
+  onPress: () => void;
+}
+
+export const Button = ({ text, styles, onPress }: ButtonProps) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [
+        globalStyles.btnPrimary,
+        {
+          opacity: pressed ? 0.8 : 1,
+          backgroundColor: colors.primary,
+        },
+        styles,
+      ]}>
+      <Text
+        style={[
+          globalStyles.btnPrimaryText,
+          {
+            color: colors.buttonTextColor,
+          },
+        ]}>
+        {text}
+      </Text>
+    </Pressable>
+  );
+};
